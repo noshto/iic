62cf316384a651150e1b45d120d554b202413139
Valerii Lider <valerii.lider@gmail.com>
2021-02-17 18:42:03 +0100

    IIC and IICSignature

---
+++ iic.go
@@ -0,0 +1,146 @@
+package iic
+
+import (
+	"crypto"
+	"fmt"
+
+	"github.com/beevik/etree"
+	"github.com/noshto/dsig/pkg/safenet"
+)
+
+// Params represents collection of parameters needed for IIC function
+type Params struct {
+	SafenetConfig *safenet.Config
+	InFile        string
+	OutFile       string
+}
+
+// WriteIIC generates IIC from given parameters, writes it into the XML and saves to outFile
+func WriteIIC(params *Params) error {
+	// Load file
+	doc := etree.NewDocument()
+	if err := doc.ReadFromFile(params.InFile); err != nil {
+		return err
+	}
+
+	// Initialize Signer
+	signer := safenet.SafeNet{}
+	if err := signer.Initialize(params.SafenetConfig); err != nil {
+		return err
+	}
+	defer signer.Finalize()
+
+	// Parse parameters
+	parsed, err := parse(doc)
+	if err != nil {
+		return err
+	}
+
+	// Generate
+	IIC, IICSignature, err := GenerateIIC(signer, parsed)
+	if err != nil {
+		return err
+	}
+
+	// Save
+	doc.FindElement("//Invoice").RemoveAttr("IIC")
+	doc.FindElement("//Invoice").CreateAttr("IIC", IIC)
+
+	doc.FindElement("//Invoice").RemoveAttr("IICSignature")
+	doc.FindElement("//Invoice").CreateAttr("IICSignature", IICSignature)
+
+	doc.IndentTabs()
+	doc.Root().SetTail("")
+
+	err = doc.WriteToFile(params.OutFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GenerateIIC generates IIC and IICSignature. Orders of parameters: TIN, IssueDateTime, InvOrdNum, BusinUnitCode, TCRCode, SoftCode, TotPrice
+func GenerateIIC(signer safenet.SafeNet, params ...interface{}) (string, string, error) {
+	plainIIC := fmt.Sprintf("%s|%s|%s|%s|%s|%s|%s", params...)
+
+	hasher := crypto.SHA256.New()
+	_, err := hasher.Write([]byte(plainIIC))
+	if err != nil {
+		return "", "", err
+	}
+	sha256IIC := hasher.Sum(nil)
+
+	IICSignature, err := signer.SignPKCS1v15(sha256IIC)
+	if err != nil {
+		return "", "", err
+	}
+	hasher = crypto.MD5.New()
+	_, err = hasher.Write(IICSignature)
+	if err != nil {
+		return "", "", err
+	}
+	IIC := hasher.Sum(nil)
+
+	return fmt.Sprintf("%x", IIC), fmt.Sprintf("%x", IICSignature), err
+}
+
+// Parse retrieves values necessary for IIC generation from given doc
+func parse(doc *etree.Document) ([]interface{}, error) {
+	IssueDateTime, err := attributeOfElement("//Invoice", "IssueDateTime", doc)
+	if err != nil {
+		return []interface{}{}, err
+	}
+	InvOrdNum, err := attributeOfElement("//Invoice", "InvOrdNum", doc)
+	if err != nil {
+		return []interface{}{}, err
+	}
+	TotPrice, err := attributeOfElement("//Invoice", "TotPrice", doc)
+	if err != nil {
+		return []interface{}{}, err
+	}
+	BusinUnitCode, err := attributeOfElement("//Invoice", "BusinUnitCode", doc)
+	if err != nil {
+		return []interface{}{}, err
+	}
+	SoftCode, err := attributeOfElement("//Invoice", "SoftCode", doc)
+	if err != nil {
+		return []interface{}{}, err
+	}
+	TCRCode, err := attributeOfElement("//Invoice", "SoftCode", doc)
+	if err != nil {
+		return []interface{}{}, err
+	}
+	TIN, err := attributeOfElement("//Seller", "IDNum", doc)
+	if err != nil {
+		return []interface{}{}, err
+	}
+
+	return []interface{}{TIN, IssueDateTime, InvOrdNum, BusinUnitCode, TCRCode, SoftCode, TotPrice}, nil
+}
+
+// AttributeOfElement returns an attribute value if it's found in given element
+func attributeOfElement(elemName string, attrName string, doc *etree.Document) (string, error) {
+	return mapElement(elemName, doc, func(elem *etree.Element) (string, error) {
+		return mapAttrib(attrName, elem, func(attr *etree.Attr) (string, error) {
+			return attr.Value, nil
+		})
+	})
+}
+
+// MapElement returns element if it's found on given document
+func mapElement(elemName string, doc *etree.Document, closure func(*etree.Element) (string, error)) (string, error) {
+	elem := doc.FindElement(elemName)
+	if elem == nil {
+		return "", fmt.Errorf("can't find element %s", elemName)
+	}
+	return closure(elem)
+}
+
+// MapAttrib returns attribute value if it's found on given element
+func mapAttrib(attrName string, elem *etree.Element, closure func(*etree.Attr) (string, error)) (string, error) {
+	attr := elem.SelectAttr(attrName)
+	if attr == nil {
+		return "", fmt.Errorf("can't find attribute %s", attrName)
+	}
+	return closure(attr)
+}
